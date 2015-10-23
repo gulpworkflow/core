09e79a9b97ad6800311a0ab3a27a3ff5c2d211ba
pbest <pbest@users.noreply.github.com>
2015-10-22 20:25:28 -0400

    config.js

--- build/js/config.js
+++ build/js/config.js
@@ -1,1 +1,12 @@
-//JSPM config file
+// JSPM config file
+// ------------------
+
+
+System.config({
+  "paths": {
+    "*": "*.js",
+    "app/*": "lib/*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
