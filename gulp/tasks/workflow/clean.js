095fe9e740cd1a4c0fce57227aa0b230661af627
pbest <paul@ripe.com>
2015-10-26 11:55:59 -0400

    setting up clean task

--- gulp/tasks/workflow/clean.js
+++ gulp/tasks/workflow/clean.js
@@ -1,10 +1,10 @@
-// Default task
+// Clean
 // -----------------------------------------------------
 // =======   
 // Config Settings for Module
 // =======  
 var yaml            = require('yamljs');
-var config          = yaml.load('gulp/config.yml').tasks;
+var config          = yaml.load('gulp/config.yml');
 // =======   
 // Dependencies
 // =======  
@@ -14,7 +14,16 @@ var gulpSequence    = require('gulp-sequence');
 // Util functions
 // =======  
 var getEnabledTasks = require('../../util/getEnabledTasks')
+// =======   
+// Tasks
+// =======
+// Clean out the "build" folder - we do this before we build all source
+// files into a locally served environment for building. 
+gulp.task('clean:build', function () {
 
-gulp.task('clean', function () {
+});
+// Clean out the "dist" folder - we do this before packaging up our project
+// for distribution on a production environment
+gulp.task('clean:dist', function () {
 
 });
--- gulp/tasks/workflow/default.js
+++ gulp/tasks/workflow/default.js
@@ -10,15 +10,18 @@ var default_task_list  = yaml.load('gulp/config.yml').default_task_list;
 // =======  
 var gulp            = require('gulp');
 var gulpSequence    = require('gulp-sequence');
-var getEnabledTasks = require('../../util/getEnabledTasks')
+//var getEnabledTasks = require('../../util/getEnabledTasks')
 
 
 // First check each task and create a dynamic object of enabled tasks
 // Each task has a boolean "enable_task" setting in the config.yml file 
 // These tasks will be run initially before the watch tasks kick in
-task_list = getEnabledTasks();
-
-
-gulp.task('default', gulpSequence('clean', ['images', 'iconFont', 'favicon'], ['twig', 'nunjucks', 'jade'],  ['jspm_lib', 'sass'], 'hypertext', 'watch', 'browserSync'));
-
+//task_list = getEnabledTasks();
+//default_task_list = JSON.stringify(default_task_list).substr(1,JSON.stringify(default_task_list).length-2);
+//default_task_list = default_task_list.replace(/"/g, "'");
+//default_task_list = JSON.parse(_default_task_list);
+//default_task_list = default_task_list.escapeSpecialChars();
+thetype = typeof default_task_list;
+console.log(thetype);
+gulp.task('default', gulpSequence('clean:build', ['images', 'iconFont', 'favicon'], ['twig', 'nunjucks', 'jade'],  ['jspm_lib', 'sass'], 'hypertext', 'watch', 'browserSync'));
 //gulp.task('default', gulpSequence(default_task_list));
