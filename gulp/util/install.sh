54dd71d3c69b3c5fdf77ce8ae5c6bf0a422cf7b1
pbest <paul@ripe.com>
2015-10-30 17:57:08 -0400

    install shell script

---
+++ gulp/util/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo -e "\n--> Download gulp-starter\n"
+curl -L https://github.com/ripestudios/gulp-starter/archive/master.zip > gulp-starter-master.zip
+
+echo -e "\n--> Extract\n"
+unzip gulp-starter-master.zip
